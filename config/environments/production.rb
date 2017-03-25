bce13f14b3faad64dabb4a1555b9e753dd6c8bd9
Nick Saindon <ntimeproductions@gmail.com>
2017-03-25 16:35:38 -0400

    Added smtp config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://stark-bastion-99106.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://itnt-blog.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    domain: 'itnt-blog.com',
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
   
 end
